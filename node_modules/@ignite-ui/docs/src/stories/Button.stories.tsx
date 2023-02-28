e6309c391b42243d248505e3f89cd90118ad7c7c
Aldinei <aldinei.franco@unimedprudente.com.br>
2023-02-28 08:11:16 -0300

    first commit

---
+++ node_modules/@ignite-ui/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,23 @@
+import { Button, ButtonProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Button',
+  component: Button,
+
+  args: {
+    children: 'Enviar',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {
+  args: {
+    size: 'small',
+  },
+}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+  },
+}
---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,12 @@
+// cores
+export * from './colors'
+
+// fontes
+export * from './font-sizes'
+export * from './font-weights'
+export * from './fonts'
+export * from './line-heights'
+
+// espaçamentos
+export * from './radii'
+export * from './space'
